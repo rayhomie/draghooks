7961ddb41a611be0f0ac65cc61db3f0b83c0a56a
rayhomie <1572801584@qq.com>
2020-11-07 10:04:30 +0800

    feat: fastdom优化

--- src/test/dist/useDraglineTest.js
+++ src/test/dist/useDraglineTest.js
@@ -8,6 +8,7 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
 };
 exports.__esModule = true;
 var react_1 = require("react");
+var fastdom_1 = require("fastdom");
 var dataSource = [1, 2, 3, 4, 5, 6];
 var UseDraglineTest = function (props) {
     var storeAllRef = react_1.useRef(null);
@@ -15,51 +16,79 @@ var UseDraglineTest = function (props) {
     var POSITIONARR = react_1.useRef([]);
     var MOUSEPOSITION = react_1.useRef([0, 0]);
     react_1.useEffect(function () {
+        //div坐标获取
         var Store = storeAllRef.current;
         var position = [];
         Store.forEach(function (v, i) {
             position.push([v.current.offsetLeft, v.current.offsetTop]);
-            v.current.style.position = 'absolute';
+            v.current.style.position = "absolute";
             v.current.style.zIndex = 0;
-            v.current.addEventListener('mousedown', function (e) { MouseDown(e, i); });
-            v.current.addEventListener('mousemove', function (e) { MouseMove(e, i); });
-            v.current.addEventListener('mouseup', function (e) { MouseUp(e, i); });
+            v.current.addEventListener("mousedown", function (e) {
+                MouseDown(e, i);
+            });
+            v.current.addEventListener("mousemove", function (e) {
+                MouseMove(e, i);
+            });
+            v.current.addEventListener("mouseup", function (e) {
+                MouseUp(e, i);
+            });
         });
         POSITIONARR.current = position;
         ISMOVE.current = isMoveArr;
     }, []);
     var MouseDown = function (e, index) {
         e.preventDefault();
-        var isMoveArr = __spreadArrays(ISMOVE.current);
-        MOUSEPOSITION.current = [e.clientX, e.clientY];
-        isMoveArr[index] = true;
-        ISMOVE.current = isMoveArr;
-        var curPos = __spreadArrays(POSITIONARR.current);
-        curPos[index][0] = storeAllRef.current[index].current.offsetLeft;
-        curPos[index][1] = storeAllRef.current[index].current.offsetTop;
-        POSITIONARR.current = curPos;
-        storeAllRef.current[index].current.style.cursor = 'move';
-        storeAllRef.current[index].current.style.zIndex = 999;
+        fastdom_1["default"].measure(function () {
+            var isMoveArr = __spreadArrays(ISMOVE.current);
+            MOUSEPOSITION.current = [e.clientX, e.clientY];
+            isMoveArr[index] = true;
+            ISMOVE.current = isMoveArr;
+            var curPos = __spreadArrays(POSITIONARR.current);
+            curPos[index][0] = storeAllRef.current[index].current.offsetLeft;
+            curPos[index][1] = storeAllRef.current[index].current.offsetTop;
+            POSITIONARR.current = curPos;
+            storeAllRef.current[index].current.style.cursor = "move";
+            storeAllRef.current[index].current.style.zIndex = 999;
+        });
     };
     var MouseMove = function (e, index) {
         e.preventDefault();
-        var isMoveArr = __spreadArrays(ISMOVE.current);
-        if (isMoveArr[index] === false) {
-            return;
-        }
-        var _a = [e.clientX, e.clientY], mouseX = _a[0], mouseY = _a[1];
-        storeAllRef.current[index].current.style.left = POSITIONARR.current[index][0] + mouseX - MOUSEPOSITION.current[0] + "px";
-        storeAllRef.current[index].current.style.top = POSITIONARR.current[index][1] + mouseY - MOUSEPOSITION.current[1] + "px";
+        fastdom_1["default"].mutate(function () {
+            var isMoveArr = __spreadArrays(ISMOVE.current);
+            if (isMoveArr[index] === false) {
+                return;
+            }
+            var _a = [e.clientX, e.clientY], mouseX = _a[0], mouseY = _a[1];
+            storeAllRef.current[index].current.style.left = POSITIONARR.current[index][0] + mouseX - MOUSEPOSITION.current[0] + "px";
+            storeAllRef.current[index].current.style.top = POSITIONARR.current[index][1] + mouseY - MOUSEPOSITION.current[1] + "px";
+        });
     };
     var MouseUp = function (e, index) {
-        var _a, _b;
         e.preventDefault();
-        var isMoveArr = __spreadArrays(ISMOVE.current);
-        isMoveArr[index] = false;
-        ISMOVE.current = isMoveArr;
-        storeAllRef.current[index].current.style.zIndex = 0;
-        storeAllRef.current[index].current.style.cursor = 'default';
-        console.log((_a = storeAllRef.current[index - 1]) === null || _a === void 0 ? void 0 : _a.current.offsetTop, storeAllRef.current[index].current.offsetTop, (_b = storeAllRef.current[index + 1]) === null || _b === void 0 ? void 0 : _b.current.offsetTop);
+        fastdom_1["default"].measure(function () {
+            var isMoveArr = __spreadArrays(ISMOVE.current);
+            isMoveArr[index] = false;
+            ISMOVE.current = isMoveArr;
+            storeAllRef.current[index].current.style.zIndex = 0;
+            storeAllRef.current[index].current.style.cursor = "default";
+        });
+        // const afterPre = storeAllRef.current[index - 1]?.current.offsetTop;
+        // const afterCur = storeAllRef.current[index].current.offsetTop;
+        // const afterNext = storeAllRef.current[index + 1]?.current.offsetTop;
+        // const beforeCur = POSITIONARR.current[index];
+        // console.log(afterPre, afterCur, afterNext, "beforeCur:", beforeCur[1]);
+        // if (afterCur <= (afterPre + beforeCur[1]) / 2 && afterCur >= afterPre) {
+        //   console.log(11);
+        //   // storeAllRef.current[index].current.style.top = `${afterPre}px`
+        //   // storeAllRef.current[index - 1].current.style.top = `${beforeCur[1]}px`
+        //   // storeAllRef.current[index].current.style.left = `${beforeCur[0]}px`
+        //   let cur = storeAllRef.current[index];
+        //   storeAllRef.current[index] = storeAllRef.current[index - 1];
+        //   storeAllRef.current[index - 1] = cur;
+        // } else {
+        //   storeAllRef.current[index].current.style.top = `${beforeCur[1]}px`;
+        //   storeAllRef.current[index].current.style.left = `${beforeCur[0]}px`;
+        // }
     };
     var isMoveArr = [];
     var allRef = [];
@@ -70,7 +99,7 @@ var UseDraglineTest = function (props) {
             return (react_1["default"].createElement("div", { key: index, style: {
                     width: 280,
                     height: 50,
-                    border: '1px solid #ccc',
+                    border: "1px solid #ccc",
                     left: 17 + "px",
                     top: index * 65 + 20 + "px"
                 }, onClick: function () { }, ref: allRef[index] }, item));
@@ -78,6 +107,6 @@ var UseDraglineTest = function (props) {
         storeAllRef.current = allRef;
         return res;
     };
-    return (react_1["default"].createElement("div", { style: { width: 300, height: 500, border: '1px solid #ccc' } }, generateList(dataSource)));
+    return (react_1["default"].createElement("div", { style: { width: 300, height: 500, border: "1px solid #ccc" } }, generateList(dataSource)));
 };
 exports["default"] = UseDraglineTest;
--- src/test/useDraglineTest.tsx
+++ src/test/useDraglineTest.tsx
@@ -1,100 +1,133 @@
-import React, { useRef, useEffect, createRef } from 'react'
+import React, { useRef, useEffect, createRef } from "react";
+import fastdom from "fastdom";
 
-interface PropTypes { }
-type position = [number, number]
-const dataSource = [1, 2, 3, 4, 5, 6]
+interface PropTypes {}
+type position = [number, number];
+const dataSource = [1, 2, 3, 4, 5, 6];
 
 const UseDraglineTest: React.FC<PropTypes> = (props) => {
-  const storeAllRef = useRef<any>(null)
-  const ISMOVE = useRef<boolean[]>([])
-  const POSITIONARR = useRef<Array<position>>([])
-  const MOUSEPOSITION = useRef<position>([0, 0])
+  const storeAllRef = useRef<any>(null);
+  const ISMOVE = useRef<boolean[]>([]);
+  const POSITIONARR = useRef<Array<position>>([]);
+  const MOUSEPOSITION = useRef<position>([0, 0]);
 
-  useEffect(() => {//div坐标获取
-    const Store = storeAllRef.current as Array<any>
-    const position: Array<position> = []
+  useEffect(() => {
+    //div坐标获取
+    const Store = storeAllRef.current as Array<any>;
+    const position: Array<position> = [];
     Store.forEach((v, i) => {
-      position.push([v.current.offsetLeft, v.current.offsetTop])
-      v.current.style.position = 'absolute'
-      v.current.style.zIndex = 0
-      v.current.addEventListener('mousedown', (e: any) => { MouseDown(e, i) })
-      v.current.addEventListener('mousemove', (e: any) => { MouseMove(e, i) })
-      v.current.addEventListener('mouseup', (e: any) => { MouseUp(e, i) })
-    })
-    POSITIONARR.current = position
-    ISMOVE.current = isMoveArr
-  }, [])
+      position.push([v.current.offsetLeft, v.current.offsetTop]);
+      v.current.style.position = "absolute";
+      v.current.style.zIndex = 0;
+      v.current.addEventListener("mousedown", (e: any) => {
+        MouseDown(e, i);
+      });
+      v.current.addEventListener("mousemove", (e: any) => {
+        MouseMove(e, i);
+      });
+      v.current.addEventListener("mouseup", (e: any) => {
+        MouseUp(e, i);
+      });
+    });
+    POSITIONARR.current = position;
+    ISMOVE.current = isMoveArr;
+  }, []);
 
   const MouseDown = (e: any, index: number) => {
-    e.preventDefault()
-    const isMoveArr = [...ISMOVE.current]
-    MOUSEPOSITION.current = [e.clientX, e.clientY]
-    isMoveArr[index] = true
-    ISMOVE.current = isMoveArr
-    const curPos = [...POSITIONARR.current]
-    curPos[index][0] = storeAllRef.current[index].current.offsetLeft
-    curPos[index][1] = storeAllRef.current[index].current.offsetTop
-    POSITIONARR.current = curPos
-    storeAllRef.current[index].current.style.cursor = 'move'
-    storeAllRef.current[index].current.style.zIndex = 999
-  }
+    e.preventDefault();
+    fastdom.measure(() => {
+      const isMoveArr = [...ISMOVE.current];
+      MOUSEPOSITION.current = [e.clientX, e.clientY];
+      isMoveArr[index] = true;
+      ISMOVE.current = isMoveArr;
+      const curPos = [...POSITIONARR.current];
+
+      curPos[index][0] = storeAllRef.current[index].current.offsetLeft;
+      curPos[index][1] = storeAllRef.current[index].current.offsetTop;
+
+      POSITIONARR.current = curPos;
+      storeAllRef.current[index].current.style.cursor = "move";
+      storeAllRef.current[index].current.style.zIndex = 999;
+    });
+  };
 
   const MouseMove = (e: any, index: number) => {
-    e.preventDefault()
-    const isMoveArr = [...ISMOVE.current]
-    if (isMoveArr[index] === false) {
-      return;
-    }
-    const [mouseX, mouseY] = [e.clientX, e.clientY]
-    storeAllRef.current[index].current.style.left = `${POSITIONARR.current[index][0] + mouseX - MOUSEPOSITION.current[0]}px`
-    storeAllRef.current[index].current.style.top = `${POSITIONARR.current[index][1] + mouseY - MOUSEPOSITION.current[1]}px`
-  }
+    e.preventDefault();
+    fastdom.mutate(() => {
+      const isMoveArr = [...ISMOVE.current];
+      if (isMoveArr[index] === false) {
+        return;
+      }
+      const [mouseX, mouseY] = [e.clientX, e.clientY];
+      storeAllRef.current[index].current.style.left = `${
+        POSITIONARR.current[index][0] + mouseX - MOUSEPOSITION.current[0]
+      }px`;
+      storeAllRef.current[index].current.style.top = `${
+        POSITIONARR.current[index][1] + mouseY - MOUSEPOSITION.current[1]
+      }px`;
+    });
+  };
 
   const MouseUp = (e: any, index: number) => {
-    e.preventDefault()
-    const isMoveArr = [...ISMOVE.current]
-    isMoveArr[index] = false
-    ISMOVE.current = isMoveArr
-    storeAllRef.current[index].current.style.zIndex = 0
-    storeAllRef.current[index].current.style.cursor = 'default'
-    console.log(storeAllRef.current[index - 1]?.current.offsetTop, storeAllRef.current[index].current.offsetTop, storeAllRef.current[index + 1]?.current.offsetTop);
+    e.preventDefault();
+    fastdom.measure(() => {
+      const isMoveArr = [...ISMOVE.current];
+      isMoveArr[index] = false;
+      ISMOVE.current = isMoveArr;
+      storeAllRef.current[index].current.style.zIndex = 0;
+      storeAllRef.current[index].current.style.cursor = "default";
+    });
+    // const afterPre = storeAllRef.current[index - 1]?.current.offsetTop;
+    // const afterCur = storeAllRef.current[index].current.offsetTop;
+    // const afterNext = storeAllRef.current[index + 1]?.current.offsetTop;
+    // const beforeCur = POSITIONARR.current[index];
+    // console.log(afterPre, afterCur, afterNext, "beforeCur:", beforeCur[1]);
+    // if (afterCur <= (afterPre + beforeCur[1]) / 2 && afterCur >= afterPre) {
+    //   console.log(11);
+    //   // storeAllRef.current[index].current.style.top = `${afterPre}px`
+    //   // storeAllRef.current[index - 1].current.style.top = `${beforeCur[1]}px`
+    //   // storeAllRef.current[index].current.style.left = `${beforeCur[0]}px`
+    //   let cur = storeAllRef.current[index];
+    //   storeAllRef.current[index] = storeAllRef.current[index - 1];
+    //   storeAllRef.current[index - 1] = cur;
+    // } else {
+    //   storeAllRef.current[index].current.style.top = `${beforeCur[1]}px`;
+    //   storeAllRef.current[index].current.style.left = `${beforeCur[0]}px`;
+    // }
+  };
 
-  }
-
-  const isMoveArr: boolean[] = []
-  const allRef: any = []
+  const isMoveArr: boolean[] = [];
+  const allRef: any = [];
   const generateList = (dataSource: any[]) => {
     let res = dataSource.map((item, index) => {
-      allRef[`${index}`] = createRef<any>()
-      isMoveArr.push(false)
+      allRef[`${index}`] = createRef<any>();
+      isMoveArr.push(false);
       return (
         <div
           key={index}
           style={{
             width: 280,
             height: 50,
-            border: '1px solid #ccc',
+            border: "1px solid #ccc",
             left: `${17}px`,
-            top: `${index * 65 + 20}px`
+            top: `${index * 65 + 20}px`,
           }}
-          onClick={() => { }}
+          onClick={() => {}}
           ref={allRef[index]}
         >
           {item}
         </div>
-      )
-    })
-    storeAllRef.current = allRef
-    return res
-  }
+      );
+    });
+    storeAllRef.current = allRef;
+    return res;
+  };
 
   return (
-    <div
-      style={{ width: 300, height: 500, border: '1px solid #ccc' }}
-    >
+    <div style={{ width: 300, height: 500, border: "1px solid #ccc" }}>
       {generateList(dataSource)}
     </div>
-  )
-}
+  );
+};
 
-export default UseDraglineTest
+export default UseDraglineTest;
